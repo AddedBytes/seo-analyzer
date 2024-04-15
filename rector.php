f30daf141ad13ad26eee32c81a3704e171baaa34
Peter Lounton <plounton@me.com>
2024-04-15 18:03:31 +0100

    ``` feat: add phpstan config and rector for code quality (composer-updates)
    - Introduced phpstan.neon.dist to configure static analysis.
    - Created rector.php for automated code refactor.
    - Updated phpunit.xml with stricter testing configurations.
    ```

---
+++ rector.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use Rector\Set\ValueObject\LevelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+
+    $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon.dist');
+
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+    ]);
+
+    $rectorConfig->skip([
+        __DIR__ . 'src/examples',
+        __DIR__ . 'src/locale',
+        __DIR__ . 'src/vendor',
+    ]);
+
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_83,
+    ]);
+
+    $rectorConfig->parallel(240, 8, 8);
+
+
+//    $rectorConfig->skip([
+//        ClassPropertyAssignToConstructorPromotionRector::class,
+//        AddOverrideAttributeToOverriddenMethodsRector::class,
+//    ]);
+
+
+    $rectorConfig::configure()->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        codingStyle: true,
+        typeDeclarations: true,
+        privatization: true,
+        naming: true,
+        instanceOf: true,
+        earlyReturn: true,
+        strictBooleans: true,
+    );
+};
