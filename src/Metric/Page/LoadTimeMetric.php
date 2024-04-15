d1e3ff66e7e4dc49f18c662a20b22c14a2d79a5d
Peter Lounton <plounton@me.com>
2024-04-15 13:20:01 +0100

    ``` feat: improve readability of LoadTimeMetric messages #composer-updates
    Adjust alignment of assignment operators and add line breaks in
    LoadTimeMetric.php for better readability of the code.
    ```

--- src/Metric/Page/LoadTimeMetric.php
+++ src/Metric/Page/LoadTimeMetric.php
@@ -20,16 +20,20 @@ class LoadTimeMetric extends AbstractMetric
         switch (true) {
             case ($this->value > 3):
                 $this->impact = 8;
-                $message = 'The site takes very long to load. You should definitely consider rebuilding the page and/or change the hosting provider';
+                $message      = 'The site takes very long to load. You should definitely consider rebuilding the page and/or change the hosting provider';
+
                 break;
             case ($this->value > 1):
                 $this->impact = 2;
-                $message = 'You should optimise your site for faster loading, as this could have strong impact on SEO';
+                $message      = 'You should optimise your site for faster loading, as this could have strong impact on SEO';
+
                 break;
             default:
                 $message = 'The site loads very fast';
+
                 break;
         }
+
         return $message;
     }
 }
