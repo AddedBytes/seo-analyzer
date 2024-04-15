10231ee24a836ee6f290c2c50976ace7a0cf5fcb
Peter Lounton <plounton@me.com>
2024-04-15 13:21:44 +0100

    ``` style: standardise array formatting in SizeMetric.php (#composer-updates)
    Ensure consistency in array element indentation and add missing commas
    after each array item in the $results and $conditions arrays.
    ```

--- src/Metric/Page/Content/SizeMetric.php
+++ src/Metric/Page/Content/SizeMetric.php
@@ -10,20 +10,20 @@ class SizeMetric extends AbstractMetric
 
     protected $results = [
         'read_error' => [
-            self::IMPACT => 10,
-            self::MESSAGE => 'Can not read your page content'
+            self::IMPACT  => 10,
+            self::MESSAGE => 'Can not read your page content',
         ],
         'empty_content' => [
-            self::IMPACT => 10,
-            self::MESSAGE => 'Looks that your site content is empty'
+            self::IMPACT  => 10,
+            self::MESSAGE => 'Looks that your site content is empty',
         ],
         'very_big' => [
-            self::IMPACT => 3,
-            self::MESSAGE => 'The site is very big. You should consider rebuilding the page to optimise its size'
+            self::IMPACT  => 3,
+            self::MESSAGE => 'The site is very big. You should consider rebuilding the page to optimise its size',
         ],
         'too_big' => [
-            self::IMPACT => 1,
-            self::MESSAGE => 'You should consider some optimisation of the page to decrease its size'
+            self::IMPACT  => 1,
+            self::MESSAGE => 'You should consider some optimisation of the page to decrease its size',
         ],
     ];
 
@@ -31,10 +31,10 @@ class SizeMetric extends AbstractMetric
     {
         parent::__construct($inputData);
         $conditions = [
-            'read_error' => $this->value === false,
+            'read_error'    => $this->value === false,
             'empty_content' => $this->value === 0,
-            'very_big' => $this->value > 80000,
-            'too_big' => $this->value > 30000,
+            'very_big'      => $this->value > 80000,
+            'too_big'       => $this->value > 30000,
         ];
         $this->setUpResultsConditions($conditions);
     }
