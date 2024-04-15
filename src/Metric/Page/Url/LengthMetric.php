9d17506105e5c6535f0a8b2ba287a1465db96805
Peter Lounton <plounton@me.com>
2024-04-15 13:19:51 +0100

    ``` feat: add spacing for readability in LengthMetric.php (#composer-updates)
    Whitespace added to improve code structure and readability in the
    LengthMetric class.
    ```

--- src/Metric/Page/Url/LengthMetric.php
+++ src/Metric/Page/Url/LengthMetric.php
@@ -15,12 +15,15 @@ class LengthMetric extends AbstractMetric
     {
         if ($this->value > 40) {
             $this->impact = 4;
+
             return "The site URL is very long. You should consider using some shorter URL";
         }
         if ($this->value > 20) {
             $this->impact = 1;
+
             return "You should consider using some shorter URL";
         }
+
         return 'The size of URL is ok';
     }
 }
