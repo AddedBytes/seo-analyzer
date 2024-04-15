f3ce1f4b3830a3f74b630ab858d65a42834681b6
Peter Lounton <plounton@me.com>
2024-04-15 13:21:23 +0100

    ``` refactor: streamline conditionals in RobotsMetric.php (composer-updates)
    Replace `else if` with separate `if` statement to improve code clarity.
    Add missing line breaks after setting impact values.
    ```

--- src/Metric/File/RobotsMetric.php
+++ src/Metric/File/RobotsMetric.php
@@ -15,11 +15,15 @@ class RobotsMetric extends AbstractMetric
     {
         if (empty($this->value)) {
             $this->impact = 1;
+
             return 'no';
-        } elseif (stripos((string) $this->value, 'Disallow: /') !== false) {
+        }
+        if (stripos((string) $this->value, 'Disallow: /') !== false) {
             $this->impact = 5;
+
             return 'Robots.txt file blocks some parts of your site from indexing by search engines. Blocking content can have a big negative impact on SEO';
         }
+
         return 'yes';
     }
 }
