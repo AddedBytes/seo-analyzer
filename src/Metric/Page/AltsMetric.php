c35441f3d79be25745610a872f47631d6796c8a5
Peter Lounton <plounton@me.com>
2024-04-15 13:17:08 +0100

    ``` refactor: standardise whitespace in AltsMetric calculation (composer-updates)
    Ensure consistent spacing around variable assignments in the AltsMetric.php.
    This improves code readability and maintains coding standards.
    ```

--- src/Metric/Page/AltsMetric.php
+++ src/Metric/Page/AltsMetric.php
@@ -17,21 +17,25 @@ class AltsMetric extends AbstractMetric
             return 'There is nothing to do here as there is no images on the site.';
         }
         $imagesCount = is_countable($this->value) ? count($this->value) : 0;
-        $altsCount = count((array) array_filter($this->value));
-        $emptyAlts = $imagesCount - $altsCount;
+        $altsCount   = count((array) array_filter($this->value));
+        $emptyAlts   = $imagesCount - $altsCount;
         switch (true) {
             case ($emptyAlts > 10):
                 $this->impact = 5;
-                $message = 'There is a lot of images without alternate texts on your site. Every image should be described with alt attribute';
+                $message      = 'There is a lot of images without alternate texts on your site. Every image should be described with alt attribute';
+
                 break;
             case ($emptyAlts > 0):
                 $this->impact = 3;
-                $message = 'You should optimise your site adding missing alt descriptions to images, as this could have strong impact on SEO';
+                $message      = 'You should optimise your site adding missing alt descriptions to images, as this could have strong impact on SEO';
+
                 break;
             default:
                 $message = 'Good! All images on site have alternate descriptions';
+
                 break;
         }
+
         return $message;
     }
 }
