c977ed14978a8e76e5649dd93b61d80e256741c2
Peter Lounton <plounton@me.com>
2024-04-15 13:21:02 +0100

    ``` style: improve readability with added line breaks in RatioMetric
    Add line breaks after impact assignments in the RatioMetric class to
    enhance code readability and maintainability. No functional changes.
    ```

--- src/Metric/Page/Content/RatioMetric.php
+++ src/Metric/Page/Content/RatioMetric.php
@@ -11,7 +11,7 @@ class RatioMetric extends AbstractMetric
     public function __construct($inputData)
     {
         $ratio = 0;
-        if (!empty($inputData['code_size'])) {
+        if (! empty($inputData['code_size'])) {
             $ratio = round($inputData['content_size'] / $inputData['code_size'] * 100);
         }
         parent::__construct($ratio);
@@ -24,12 +24,15 @@ class RatioMetric extends AbstractMetric
     {
         if ($this->value < 10) {
             $this->impact = 8;
+
             return 'Content to code ratio is too low. Consider adding more text to your page or remove some unnecessary html code';
         }
         if ($this->value < 20) {
             $this->impact = 5;
+
             return 'Consider adding more text to your page or remove unnecessary html code. Good content have crucial impact on SEO';
         }
+
         return 'Page has good content to code ratio';
     }
 }
